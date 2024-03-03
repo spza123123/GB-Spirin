88c6b459c8a2371aa1dca6a4b5aad0d3256d2510
Zakhar <info@zspirin.ru>
2024-03-04 01:20:26 +0300

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Ackermann(2 ,3);
+
+int Ackermann(uint m, uint n)
+        {
+            if (m == 0)
+                return  (int) (n+1);
+            if (m > 0 && n == 0)
+                return Ackermann(m - 1, 1);
+            if (m > 0 && n > 0)
+                return Ackermann(m - 1, (uint)Ackermann(m, n - 1));
+            else
+            {
+                return -1;
+            }
+        }
+        int result = Ackermann(1,1);
+        Console.Write(result);
+        Console.Write("\n");
