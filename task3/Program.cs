1c7e1c297534f04c87ada92dd79220e7d86fcdf0
Zakhar <info@zspirin.ru>
2024-03-04 01:21:29 +0300

    Task3

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿void Main()
+{
+    int[] array = [1, 5, 76, 2, 9, 3];
+    var str = string.Join(" ", array);
+    System.Console.WriteLine(str);
+    System.Console.WriteLine();
+    int i = 0;
+    asd(array, i);
+}
+Main();
+
+void asd (int [] array,int i)
+{
+    System.Console.WriteLine(array[array.Length -1 - i]);
+    i++;
+    if( i >= array.Length)
+    {
+        return;
+    }
+    asd(array, i);
+    
+    
+
+}
